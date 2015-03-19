fc0c5592576afccc9bea7ca17f3120c2403bc81d
Benjamin Michotte <bmichotte@gmail.com>
2015-03-19 11:17:11 +0100

    Optimisation in cards import

--- app/utilities/database_generator.rb
+++ app/utilities/database_generator.rb
@@ -7,12 +7,8 @@ class DatabaseGenerator
   Log = Motion::Log
 
   def self.init_database(&block)
-    Dispatch::Queue.main.async do
-      database = DatabaseGenerator.new
-      database.load
-
-      block.call if block
-    end
+    database = DatabaseGenerator.new
+    database.load(&block)
   end
 
   def database_need_genaration
@@ -27,76 +23,87 @@ class DatabaseGenerator
   end
 
   # save all cards if Card model is empty
-  def load
-    return unless database_need_genaration
+  def load(&block)
+     unless database_need_genaration
+       block.call if block
+       return
+     end
 
     Card.destroy_all
     Mechanic.destroy_all
 
-    langs          = %w(deDE enGB enUS esES esMX frFR itIT koKR plPL ptBR ptPT ruRU zhCN zhTW)
-    valid_card_set = [
-        'Basic',
-        'Classic',
-        'Reward',
-        'Promotion',
-        'Curse of Naxxramas',
-        'Goblins vs Gnomes'
-    ]
+    # do all the creation in background
+    cdq.background do
+      langs          = %w(deDE enGB enUS esES esMX frFR itIT koKR plPL ptBR ptPT ruRU zhCN zhTW)
+      valid_card_set = [
+          'Basic',
+          'Classic',
+          'Reward',
+          'Promotion',
+          'Curse of Naxxramas',
+          'Goblins vs Gnomes'
+      ]
 
-    langs.each do |lang|
-      Log.verbose "#{lang} -> #{"cards/cardsDB.#{lang}.json".resource_path}"
-      data  = NSData.read_from "cards/cardsDB.#{lang}.json".resource_path
-      cards = JSON.parse data
+      langs.each do |lang|
+        Log.verbose "#{lang} -> #{"cards/cardsDB.#{lang}.json".resource_path}"
+        data  = NSData.read_from "cards/cardsDB.#{lang}.json".resource_path
+        cards = JSON.parse data
 
-      valid_card_set.each do |card_set|
-        cards[card_set].each do |card|
+        valid_card_set.each do |card_set|
+          cards[card_set].each do |card|
 
-          cost = card['cost']
-          # "fake" the coin... in the game files, Coin cost is empty
-          # so we set it to 0
-          if card['id'] == 'GAME_005'
-            cost = 0
-          end
-
-          rarity = card['rarity']
-          unless rarity.nil?
-            rarity = rarity._
-          end
-
-          type = card['type']
-          unless type.nil?
-            type = type._
-          end
-
-          c = Card.create(
-              :lang         => lang,
-              :name         => card['name'],
-              :card_id      => card['id'],
-              :card_type    => type,
-              :text         => card['text'],
-              :player_class => card['playerClass'],
-              :rarity       => rarity,
-              :faction      => card['faction'],
-              :flavor       => card['flavor'],
-              :how_to_get   => card['howToGet'],
-              :artist       => card['artist'],
-              :cost         => cost,
-              :collectible  => card['collectible']
-          )
-
-          card['mechanics'].each do |mechanic|
-            m = Mechanic.where(:value => mechanic).first
-            if m.nil?
-              m = Mechanic.create :value => mechanic
+            cost = card['cost']
+            # "fake" the coin... in the game files, Coin cost is empty
+            # so we set it to 0
+            if card['id'] == 'GAME_005'
+              cost = 0
             end
-            c.mechanics << m
-          end unless card['mechanics'].nil?
+
+            rarity = card['rarity']
+            unless rarity.nil?
+              rarity = rarity._
+            end
+
+            type = card['type']
+            unless type.nil?
+              type = type._
+            end
+
+            c = Card.create(
+                :lang         => lang,
+                :name         => card['name'],
+                :card_id      => card['id'],
+                :card_type    => type,
+                :text         => card['text'],
+                :player_class => card['playerClass'],
+                :rarity       => rarity,
+                :faction      => card['faction'],
+                :flavor       => card['flavor'],
+                :how_to_get   => card['howToGet'],
+                :artist       => card['artist'],
+                :cost         => cost,
+                :collectible  => card['collectible']
+            )
+
+            card['mechanics'].each do |mechanic|
+              m = Mechanic.where(:value => mechanic).first
+              if m.nil?
+                m = Mechanic.create :value => mechanic
+              end
+              c.mechanics << m
+            end unless card['mechanics'].nil?
+          end
         end
+        cdq.save(always_wait: true)
+      end
+
+      Dispatch::Queue.main.async do
+        NSUserDefaults.standardUserDefaults.setObject(DATABASE_VERSION, forKey: 'database_version')
+        cdq.save
+
+        block.call if block
       end
     end
-
-    NSUserDefaults.standardUserDefaults.setObject(DATABASE_VERSION, forKey: 'database_version')
-    cdq.save
   end
 
 end
