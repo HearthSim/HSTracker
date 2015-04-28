0630098e31af116b03caa2fd3a83c00c1ec02e37
Benjamin Michotte <bmichotte@gmail.com>
2015-04-28 18:53:27 +0200

    Change version checker to allow adding prerelease
    This will allow dev release without notifying people

--- app/utilities/version_checker.rb
+++ app/utilities/version_checker.rb
@@ -16,28 +16,40 @@ class VersionChecker
         next
       end
 
-      release_version = doc.firstNodeForXPath("//ul[contains(@class,'tag-references')]//span[contains(@class,'css-truncate-target')]",
-                                              error: error)
+      release_version = doc.nodesForXPath("//div[contains(@class, 'release-meta')]",
+                                          error: error)
       if error[0]
         Log.error error[0].description
         next
       end
       unless release_version.nil?
-        release_version = release_version.stringValue
+        release_version.each do |version|
+          span = version.elementsForName 'span'
+          if span
+            attr = span.first.attributeForName 'class'
+            if attr and attr.stringValue.include? 'release-label' and !attr.stringValue.include? 'prerelease'
+              version_number = version.firstNodeForXPath("ul/li/a/span[contains(@class, 'css-truncate-target')]", error: error)
+              if version_number
+                release_version = version_number.stringValue
+                dict          = NSBundle.mainBundle.infoDictionary
+                local_version = "#{dict['CFBundleShortVersionString']}.#{dict['CFBundleVersion']}"
 
-        dict          = NSBundle.mainBundle.infoDictionary
-        local_version = "#{dict['CFBundleShortVersionString']}.#{dict['CFBundleVersion']}"
+                Motion::Log.verbose "last release is #{release_version} -> local is #{local_version}"
 
-        Motion::Log.verbose "last release is #{release_version} -> local is #{local_version}"
+                if release_version.compare(local_version, options: NSNumericSearch) == NSOrderedDescending
+                  response = NSAlert.alert('Update'._,
+                                           :buttons     => ['OK'._, 'Cancel'._],
+                                           :informative => 'A new version of HSTracker is available, click OK to download it.'._
+                  )
 
-        if release_version.compare(local_version, options: NSNumericSearch) == NSOrderedDescending
-          response = NSAlert.alert('Update'._,
-                                   :buttons     => ['OK'._, 'Cancel'._],
-                                   :informative => 'A new version of HSTracker is available, click OK to download it.'._
-          )
+                  if response == NSAlertFirstButtonReturn
+                    NSWorkspace.sharedWorkspace.openURL(KReleasePageUrl.nsurl)
+                  end
+                end
 
-          if response == NSAlertFirstButtonReturn
-            NSWorkspace.sharedWorkspace.openURL(KReleasePageUrl.nsurl)
+                break
+              end
+            end
           end
         end
       end
