8258bd0fa63bb818632e33f3928be40b4528f81c
Benjamin Michotte <bmichotte@gmail.com>
2015-06-09 11:00:39 +0200

    Correct cards sorting

--- app/categories/ns_array.rb
+++ app/categories/ns_array.rb
@@ -2,16 +2,16 @@ class NSArray
 
   # [160, 210, 242].nscolor => 0xA0D2F2.nscolor
   def nscolor(alpha=1.0)
-    red = self[0] / 255.0
+    red   = self[0] / 255.0
     green = self[1] / 255.0
-    blue = self[2] / 255.0
+    blue  = self[2] / 255.0
     if self[3]
       alpha = self[3]
     end
     if NSColor.respond_to? 'colorWithRed:green:blue:alpha'
-      NSColor.colorWithRed(red, green:green, blue:blue, alpha:alpha.to_f)
+      NSColor.colorWithRed(red, green: green, blue: blue, alpha: alpha.to_f)
     else
-      NSColor.colorWithCalibratedRed(red, green:green, blue:blue, alpha:alpha.to_f)
+      NSColor.colorWithCalibratedRed(red, green: green, blue: blue, alpha: alpha.to_f)
     end
   end
 
@@ -23,12 +23,15 @@ class NSArray
   # sort a deck
   # sort by
   # 1) card cost
-  # 2) card type (spell, minion, ...)
-  # 3) card name
+  # 2) class card
+  # 3) card type (spell, minion, ...)
+  # 4) card name
   def sort_cards!
     sort! do |a, b|
       if a.cost != b.cost
         a.cost <=> b.cost
+      elsif (a.player_class.nil? and !b.player_class.nil?) or (!a.player_class.nil? and b.player_class.nil?)
+        a.player_class.nil? ? 1 : -1
       elsif a.card_type != b.card_type
         b.card_type <=> a.card_type
       else
@@ -36,6 +39,5 @@ class NSArray
       end
     end
   end
-
 
 end
