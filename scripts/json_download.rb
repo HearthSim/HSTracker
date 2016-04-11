cad97ad1275223b33315096448422b07bf036cba
Benjamin Michotte <bmichotte@gmail.com>
2016-04-11 15:59:30 +0200

    update cards

--- scripts/json_download.rb
+++ scripts/json_download.rb
@@ -3,15 +3,13 @@
 require 'rubygems'
 require 'open-uri'
 
-HS_VERSION = 12051
-
 cards_dir = "#{File.dirname(__FILE__)}/../HSTracker/Resources/Cards"
-langs = %w(deDE esES itIT ptBR zhTW esMX koKR ruRU enUS frFR plPL zhCN jaJP)
+langs = %w(deDE esES itIT ptBR zhTW esMX koKR ruRU enUS frFR plPL zhCN jaJP thTH)
 langs.each do |lang|
   open("#{cards_dir}/cardsDB.#{lang}.json", 'wb') do |file|
     puts "Downloading #{lang}/cards.json to cardsDB.#{lang}.json"
 
-    url = "https://api.hearthstonejson.com/v1/#{HS_VERSION}/#{lang}/cards.json"
+    url = "https://api.hearthstonejson.com/v1/latest/#{lang}/cards.json"
     file << open(url).read
   end
 end
