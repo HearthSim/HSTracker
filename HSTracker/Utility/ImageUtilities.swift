7e954be3aeaee122e6965a524fb4ba03d89cac6f
Benjamin Michotte <bmichotte@gmail.com>
2016-06-20 20:49:48 +0200

    remove unused code

--- HSTracker/Utility/ImageUtilities.swift
+++ HSTracker/Utility/ImageUtilities.swift
@@ -13,32 +13,12 @@ struct ImageUtilities {
     static func screenshotPlayerRank() -> NSImage? {
         let hearthstoneWindow = SizeHelper.hearthstoneWindow
         if let image = hearthstoneWindow.screenshot() {
-            Log.verbose?.message("\(image)")
-            if let scaled = resize(image, size: NSSize(width: SizeHelper.BaseWidth,
-                height: SizeHelper.BaseHeight)) {
-            Log.verbose?.message("resize : \(scaled)")
-            let cropped = cropRect(scaled, rect: NSRect(x: 31, y: 112, width: 24, height: 24))
-            Log.verbose?.message("cropped : \(cropped)")
-            return cropped
-            }
-        }
-        Log.verbose?.message("! image")
-        return nil
-    }
-    
-    static func screenshotPlayerRankArea() -> NSImage? {
-        let hearthstoneWindow = SizeHelper.hearthstoneWindow
-        if let image = hearthstoneWindow.screenshot() {
-            Log.verbose?.message("\(image)")
             let cropped = cropRect(image,
                                    rect: NSRect(x: 0, y: 0,
                                     width: image.size.width / 5,
                                     height: image.size.height / 3))
-            Log.verbose?.message("cropped : \(cropped)")
             return cropped
-            
         }
-        Log.verbose?.message("! image")
         return nil
     }
 
@@ -52,27 +32,6 @@ struct ImageUtilities {
                            fraction:1.0)
         target.unlockFocus()
         return target
-    }
-    
-    static func resizeImage(image: NSImage) -> NSImage {
-        let height = SizeHelper.BaseHeight
-        
-        if image.size.height == height {
-            return image
-        }
-        let ratio = 4.0 / 3.0
-        let width = CGFloat(Double(height) * ratio)
-        let cropWidth = CGFloat(Double(image.size.height) * ratio)
-        let scaled = NSImage(size: NSSize(width: width, height: height))
-        scaled.lockFocus()
-        image.size = NSSize(width: cropWidth, height: image.size.height)
-        NSGraphicsContext.currentContext()?.imageInterpolation = .High
-        image.drawAtPoint(NSZeroPoint,
-                                fromRect: NSRect(x: 0, y: 0, width: width, height: height),
-                                operation: .CompositeCopy,
-                                fraction:1.0)
-        scaled.unlockFocus()
-        return scaled
     }
     
     static func resize(origImage: NSImage, size: NSSize) -> NSImage? {
