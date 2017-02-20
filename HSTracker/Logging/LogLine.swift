e89ddd601ae378bee317fdf6c0938fb8c9925aa3
Istvan Fehervari <gooksl@gmail.com>
2017-02-20 18:23:48 +0100

    Fixed memory consumption bug

--- HSTracker/Logging/LogLine.swift
+++ HSTracker/Logging/LogLine.swift
@@ -33,37 +33,39 @@ struct LogLine {
     }
     
     static func parseTime(line: String) -> (DateInRegion, Double) {
-        guard line.characters.count > 20 else { return (DateInRegion(), 0) }
-
-        guard let fromLine = line.substringWithRange(2, location: 16)
-            .components(separatedBy: " ").first else { return (DateInRegion(), 0) }
-        
-        guard !fromLine.isEmpty else { return (DateInRegion(), 0) }
-        let components = fromLine.components(separatedBy: ".")
-        guard [1, 2].contains(components.count) else { return (DateInRegion(), 0) }
-
-        guard let dateTime = try? DateInRegion(string: components[0],
-                                            format: .custom("HH:mm:ss")) else {
-            return (DateInRegion(), 0)
-        }
-
-        var nanoseconds: Double = 0
-        if components.count == 2 && components[1].characters.count >= 3 {
-            if let milliseconds = Double(components[1]) {
-                nanoseconds = milliseconds
+        return autoreleasepool { () -> (DateInRegion, Double) in
+            guard line.characters.count > 20 else { return (DateInRegion(), 0) }
+            
+            guard let fromLine = line.substringWithRange(2, location: 16)
+                .components(separatedBy: " ").first else { return (DateInRegion(), 0) }
+            
+            guard !fromLine.isEmpty else { return (DateInRegion(), 0) }
+            let components = fromLine.components(separatedBy: ".")
+            guard [1, 2].contains(components.count) else { return (DateInRegion(), 0) }
+            
+            guard let dateTime = try? DateInRegion(string: components[0],
+                                                   format: .custom("HH:mm:ss")) else {
+                                                    return (DateInRegion(), 0)
             }
+            
+            var nanoseconds: Double = 0
+            if components.count == 2 && components[1].characters.count >= 3 {
+                if let milliseconds = Double(components[1]) {
+                    nanoseconds = milliseconds
+                }
+            }
+            
+            guard let date = try? DateInRegion().atTime(hour: dateTime.hour,
+                                                        minute: dateTime.minute,
+                                                        second: dateTime.second) else {
+                                                            return (dateTime, nanoseconds)
+            }
+            var dateInRegion = date
+            if dateInRegion.isInFuture {
+                dateInRegion = dateInRegion - 1.day
+            }
+            return (dateInRegion, nanoseconds)
         }
-
-        guard let date = try? DateInRegion().atTime(hour: dateTime.hour,
-                                                    minute: dateTime.minute,
-                                                    second: dateTime.second) else {
-                return (dateTime, nanoseconds)
-        }
-        var dateInRegion = date
-        if dateInRegion.isInFuture {
-            dateInRegion = dateInRegion - 1.day
-        }
-        return (dateInRegion, nanoseconds)
     }
 }
 
