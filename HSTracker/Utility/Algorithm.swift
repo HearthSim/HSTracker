1bdb9572b8abb7723ca73b35035c50d669fb80fe
Istvan Fehervari <gooksl@gmail.com>
2017-03-20 21:39:20 +0100

    Added linkedlist and thread-safe queue implementations

---
+++ HSTracker/Utility/Algorithm.swift
@@ -0,0 +1,137 @@
+//
+//  Algorithm.swift
+//  HSTracker
+//
+//  Created by Istvan Fehervari on 20/03/2017.
+//  Copyright © 2017 Benjamin Michotte. All rights reserved.
+//
+
+import Foundation
+
+public class Node<T> {
+    var value: T
+    
+    var next: Node<T>?
+    weak var previous: Node<T>?
+    
+    init(value: T) {
+        self.value = value
+    }
+}
+
+public class LinkedList<T> {
+    
+    fileprivate var head: Node<T>?
+    private var tail: Node<T>?
+    private var _count: Int = 0
+    
+    public var isEmpty: Bool {
+        return head == nil
+    }
+    
+    public var first: Node<T>? {
+        return head
+    }
+    
+    public var last: Node<T>? {
+        return tail
+    }
+    
+    public func append(value: T) {
+        
+        let newNode = Node(value: value)
+        
+        if let tailNode = tail {
+            newNode.previous = tailNode
+            tailNode.next = newNode
+        } else {
+            head = newNode
+        }
+        
+        tail = newNode
+        _count += 1
+    }
+    
+    public func nodeAt(index: Int) -> Node<T>? {
+
+        if self._count <= index {
+            return nil
+        }
+        
+        if index >= 0 {
+            var node = head
+            var i = index
+
+            while node != nil {
+                if i == 0 { return node }
+                i -= 1
+                node = node!.next
+            }
+        }
+
+        return nil
+    }
+    
+    public func clear() {
+        head = nil
+        tail = nil
+    }
+    
+    public func remove(node: Node<T>) -> T {
+        let prev = node.previous
+        let next = node.next
+        
+        if let prev = prev {
+            prev.next = next
+        } else {
+            head = next
+        }
+        next?.previous = prev
+        
+        if next == nil {
+            tail = prev
+        }
+        
+        self._count -= 1
+        
+        node.previous = nil 
+        node.next = nil
+        return node.value
+    }
+    
+    public func remove(at: Int) {
+        if let node = nodeAt(index: at) {
+            let _ = remove(node: node)
+        }
+    }
+    
+    public func count() -> Int {
+        return self._count
+    }
+}
+
+/**
+ * Thread-safe queue implementation
+ */
+public class ConcurrentQueue<T> {
+
+    private var elements = LinkedList<T>()
+    private let accessQueue = DispatchQueue(label: "SynchronizedQueueAccess", attributes: .concurrent)
+    
+    public func append(value: T) {
+        self.accessQueue.async(flags:.barrier) {
+            self.elements.append(value: value)
+        }
+    }
+    
+    public func removeAtIndex(index: Int) {
+        
+        self.accessQueue.async(flags:.barrier) {
+            self.elements.remove(at: index)
+        }
+    }
+    
+    public func count() -> Int {
+        return elements.count()
+    }
+}
