13af2ff34ed0341d244688ed1d7169f2fd41c62f
Martin Bonnin <martin@mbonnin.net>
2020-06-13 23:08:55 +0200

    Use kotlin_hslog version 446

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,9 +3,3 @@ git clone -b macos-test https://github.com/HearthSim/HDT-Localization Translatio
 
 # Download cards
 ./scripts/cards_download.sh
-
-# Clone Arcane-Tracker for kotlin-hslog
-git clone https://github.com/HearthSim/Arcane-Tracker Arcane-Tracker
-
-# build the framework file
-./Arcane-Tracker/gradlew -p Arcane-Tracker linkReleaseFrameworkMacosX64
