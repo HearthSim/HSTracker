84c229761ed127995482ea8895a30ac58a6faf3b
Francisco Moraes <fmoraes@us.ibm.com>
2021-04-30 23:17:53 -0400

    Fix issue with Jaraxxus not updating tracker (issue #1219)

--- HSTracker/Logging/Entity.swift
+++ HSTracker/Logging/Entity.swift
@@ -109,7 +109,7 @@ class Entity {
         return self[.cardtype] == CardType.hero_power.rawValue
     }
     var isPlayableHero: Bool {
-        return isHero && card.set != .core && card.set != .hero_skins && card.collectible
+        return isHero && card.set != .hero_skins && card.collectible
     }
 
     var isInHand: Bool {
