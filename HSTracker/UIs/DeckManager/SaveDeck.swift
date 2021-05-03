5593180ac96d224f54861e50da6e9fbcedcc68ed
Francisco Moraes <fmoraes@us.ibm.com>
2021-05-03 08:22:21 -0400

    Fix compiler warning

--- HSTracker/UIs/DeckManager/SaveDeck.swift
+++ HSTracker/UIs/DeckManager/SaveDeck.swift
@@ -10,7 +10,7 @@ import Foundation
 import RealmSwift
 import AppKit
 
-protocol SaveDeckDelegate: class {
+protocol SaveDeckDelegate: AnyObject {
     func deckSaveSaved()
     func deckSaveCanceled()
 }
