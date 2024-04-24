26c84c5a985e28672341722b0a8d0ecb007a9336
Francisco Moraes <francisco.moraes@gmail.com>
2024-04-24 07:20:15 -0400

    fix(Battlegrounds): crash when loading comps

--- HSTracker/UIs/Battlegrounds/Composition/BattlegroundsCompositionPopularityViewModel.swift
+++ HSTracker/UIs/Battlegrounds/Composition/BattlegroundsCompositionPopularityViewModel.swift
@@ -13,7 +13,7 @@ class BattlegroundsCompositionPopularityViewModel {
         let top3Comps = compsData.sorted(by: { $0.popularity > $1.popularity}).take(3)
         if top3Comps.count > 0 {
             let maxVal = Double.maximum(top3Comps[0].popularity, 40.0)
-            top3Compositions = top3Comps.compactMap { x in
+            top3Compositions = top3Comps.filter { compData in compData.key_minions_top3.count > 0 }.compactMap { x in
                 return BattlegroundsCompositionPopularityRowViewModel(name: x.name, minionDbfId: x.key_minions_top3[0], available: x.is_valid, popularity: x.popularity, maxPopularity: maxVal)
             }
         } else {
