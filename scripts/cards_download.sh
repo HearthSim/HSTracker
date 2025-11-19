ab33a5a0f15f92330833262f2cebe141231d082b
Alex Zeier <alex@hearthsim.net>
2025-11-19 11:25:05 -0800

    ci: run release on tag push

--- scripts/cards_download.sh
+++ scripts/cards_download.sh
@@ -3,6 +3,8 @@
 BASEDIR="$(dirname "$0")"
 HSJSON="https://api.hearthstonejson.com/v1"
 
+mkdir -p "$BASEDIR/../HSTracker/Resources/Cards"
+
 for lang in deDE enUS esES esMX frFR itIT jaJP koKR plPL ptBR ruRU thTH zhCN zhTW; do
 	echo "Downloading cards.$lang.json"
 	wget "$HSJSON/latest/$lang/cards.json" -O "$BASEDIR/../HSTracker/Resources/Cards/cardsDB.$lang.json"
